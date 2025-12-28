da3981226d4afab52701ffea3220f42bc69dab50
Ankit Lenka <ankitlenka121@gmail.com>
2025-12-28 16:05:00 +0530

    hello

--- arrlist.java
+++ arrlist.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class arrlist {
+
+
+static void main(String[] args){
+    ArrayList<Integer> ali1=new ArrayList<>(20);
+    ArrayList<Integer> ali2=new ArrayList<>(List.of(50,60,70,80,90));
+
+    ali1.add(10);
+ali1.add(0,5);
+ali1.addAll(1,ali2);
+System.out.println(ali1);
+System.out.println(ali1.contains(50));
+}
 }
