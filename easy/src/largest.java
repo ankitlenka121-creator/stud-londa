c0a334bdae51dae3a33af52b9eff80d24601a7ff
Ankit Lenka <ankitlenka121@gmail.com>
2026-05-22 23:33:31 +0530

    lio

--- easy/src/largest.java
+++ easy/src/largest.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 
 public class largest {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
         Scanner pew=new Scanner(System.in);
         int n=pew.nextInt();
         int[] a = new int[n];
         int i;
         for ( i=0;i<n;i++){
-a[i]=pew.nextInt();}
-             int largest=a[0];
-            for(i=0;i<n;i++){
-                if(a[i]>largest)
-                 largest=a[i];
-
-            }
-          System.out.println ("largest no= " + largest);
+            a[i]=pew.nextInt();
         }
+
+        int largest=a[0];
+        for(i=0;i<n;i++) {
+            if(a[i]>largest)
+                largest=a[i];
+
+        }
+        System.out.println ("largest no= " + largest);
     }
+}
 
