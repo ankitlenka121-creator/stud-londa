aed21f2d1dbc8e6a31b35da4bbcf906b0b0ae4a8
Ankit Lenka <ankitlenka121@gmail.com>
2026-05-24 23:53:41 +0530

    potato

--- easy/src/checkingsort.java
+++ easy/src/checkingsort.java
@@ -6,6 +6,7 @@ public class checkingsort {
         int n,i;
          n=oip.nextInt();
         int[] j=new int[n];
+
         for (i=0;i<n;i++){
             j[i] = oip.nextInt();}
             for (i=1;i<n;i++) {
